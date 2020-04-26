8b695de89dcb20e2b14b4c9e675c89bf1dca2556
gopisu <malgorzata.pisula@gmail.com>
2020-04-26 17:40:46 +0200

    .

--- 082_Class.py
+++ 082_Class.py
@@ -1,12 +1,15 @@
 class Cake:
     known_types = ['cake', 'muffin', 'ice cream', 'macaroni', 'pancake', 'christmas', 'pretzel', 'other']
-
-    def __init__(self, name, kind, taste, additives, filling):
+    bakery_offer = []
+    
+    def __init__(self, name, kind, taste, additives, filling, gluten_free=False):
         self.name = name
         self.kind = kind if kind in self.known_types else "other"
         self.taste = taste
         self.additives = additives.copy()
         self.filling = filling
+        self.bakery_offer.append(self)
+        self.__gluten_free = gluten_free
 
     def show_additives(self):
         if self.additives and self.additives is not []:
@@ -18,6 +21,7 @@ class Cake:
         print(f"{self.name.upper()} \nKind:   {self.kind}\nTaste:  {self.taste}")
         self.show_additives()
         print(f"Filling: {self.filling}")
+        print(f"Alergies info: Gluten free") if self._Cake__gluten_free is True else print("Alergies info: Conatins GLUTEN")
         print("--------------------")
 
     def set_filling(self, filling):
@@ -28,7 +32,7 @@ class Cake:
             self.additives.append(additive)
 
 
-pistacchio_macaroni = Cake("pistacchio macaroni", "macaroni", "sweet", [], "pistacchio")
+pistacchio_macaroni = Cake("pistacchio macaroni", "macaroni", "sweet", [], "pistacchio", gluten_free=True)
 chocolate_ice_cream = Cake("chocolate ice cream", "ice cream", "sweet", [], "chocolate")
 vanilla_pancake = Cake(
     "vanilla pancake", "pancake", "sweet", ["cherry", "chocolate"], "vanilla mousse"
@@ -45,5 +49,16 @@ cake04 = Cake('Cocoa waffle','waffle','cocoa',[],'cocoa')
 bakery_offer = [pistacchio_macaroni, cake04, chocolate_ice_cream, vanilla_pancake]
 
 print("Today in our offer:")
-for cake in bakery_offer:
+for cake in Cake.bakery_offer:
     cake.show_info()
+
+print(isinstance(cake04, Cake))
+print(type(cake04))
+
+print(vars(cake04))
+print(vars(Cake))
+
+print('-'*20)
+print(dir(cake04))
+print(dir(Cake))
+
